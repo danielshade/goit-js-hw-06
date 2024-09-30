4d59b1a95b05f02b978358e493a6b9f90cb3bea1
danielshade <113280712+danielshade@users.noreply.github.com>
2024-09-30 21:26:25 +0300

    update index html file

---
+++ js/task-2.js
@@ -0,0 +1,31 @@
+"use strict";
+
+class Storage {
+  #items = [];
+  constructor(items) {
+    this.#items = items;
+  }
+  getItems() {
+    return this.#items;
+  }
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    const deleteItem = this.#items.filter((item) => item !== itemToRemove);
+    this.#items = deleteItem;
+  }
+}
+
+//
+//
+//
+//
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
